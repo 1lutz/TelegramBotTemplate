26bbff247d2b387c843cacdc661c7c27607f863f
1lutz <lutzgog@gmail.com>
2021-02-28 20:31:52 +0100

    "using System.ComponentModel;" to easily add descriptions

--- Services/HelloWorldDialog.cs
+++ Services/HelloWorldDialog.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using TelegramBotTemplate.Models;
